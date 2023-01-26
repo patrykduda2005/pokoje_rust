00000ce1fb6701562cf17378b1359c641641b1b9
patrykduda2005 <patrykduda7@gmail.com>
2023-01-26 19:18:42 +0100

    czas na problemy borrow checkera

--- src/main.rs
+++ src/main.rs
@@ -1,123 +1,73 @@
 use std::io;
 use clearscreen;
 
-
-fn dialog(n: usize) {
-    let dialogi = [
-        "Jesteś managerem życia. Przed tobą 3 puste pokoje.\nMusisz zalokować 6 osób: Kromke, Szaraka, Mumina, Lechańskiego, Forzaoba i Dude.",
-        "Do jakiego pokoju chcesz wrzucić Kromke? (1,2 lub 3)",
-        "Do jakiego pokoju chcesz wrzucić Szaraka? (1,2 lub 3)",
-        "Do jakiego pokoju chcesz wrzucić Mumina? (1,2 lub 3)",
-        "Do jakiego pokoju chcesz wrzucić Lechańskiego? (1,2 lub 3)",
-        "Do jakiego pokoju chcesz wrzucić Forzaoba? (1,2 lub 3)",
-        "Do jakiego pokoju chcesz wrzucić Dude? (1,2 lub 3)",
-    ];
-    println!("{}", dialogi[n]);
-}
-/*
-fn wez_pozycje(n_zapytania: usize, pokoje: &[[&str;2];3]) -> usize {
-    loop {
-        let mut input = String::new();
-        dialog(n_zapytania);
-        wyswietl_pokoje(&pokoje);
-
-        io::stdin().read_line(&mut input).expect("To nie jest ciag znakow");
-        clearscreen::clear().expect("Failed to clear terminal");
-
-
-        match input.trim().parse::<usize>() {
-            Ok(v) => if !(1..=3).contains(&v)  {
-                        println!{"Podana liczba nie mieści sie w zasięgu <1,3>"}
-                    } else if !(pokoje[v-1][0] == "") && !(pokoje[v-1][1] == "") {
-                        println!{"Nie ma wolnego pokoju na tej pozycji"}
-                    } else {
-                        break v-1
-                    },
-            Err(e) => println!("Liczba powinna być całkowita: {e}")
-        }
-    }
-}
-
-fn wyswietl_pokoje(pokoje: &[[&str;2];3]) {
-    println!("-----------------POKOJE---------------------");
-    print!("|");
-    for pokoj in pokoje.iter() {
-        print!("\"{}\",\"{}\"|", pokoj[0], pokoj[1]);
-    }
-    println!("\n--------------------------------------------");
-}
-
-fn main() {
-    let mut pokoje: [[&str;2];3] = [["",""],["",""],["",""]];
-    dialog(0);
-
-    let pozycja: usize = wez_pozycje(1, &pokoje);
-    if pokoje[pozycja][0] == "" {
-        pokoje[pozycja][0] = "Kromka"
-    } else if pokoje[pozycja][1] == "" {
-        pokoje[pozycja][1] = "Kromka"
-    }
-
-    let pozycja = wez_pozycje(2, &pokoje);
-    if pokoje[pozycja][0] == "" {
-        pokoje[pozycja][0] = "Szarak"
-    } else if pokoje[pozycja][1] == "" {
-        pokoje[pozycja][1] = "Szarak"
-    }
-
-    let pozycja = wez_pozycje(3, &pokoje);
-    if pokoje[pozycja][0] == "" {
-        pokoje[pozycja][0] = "Mumin"
-    } else if pokoje[pozycja][1] == "" {
-        pokoje[pozycja][1] = "Mumin"
-    }
-
-    let pozycja = wez_pozycje(4, &pokoje);
-    if pokoje[pozycja][0] == "" {
-        pokoje[pozycja][0] = "Lechański"
-    } else if pokoje[pozycja][1] == "" {
-        pokoje[pozycja][1] = "Lechański"
-    }
-
-    let pozycja = wez_pozycje(5, &pokoje);
-    if pokoje[pozycja][0] == "" {
-        pokoje[pozycja][0] = "Forzaob"
-    } else if pokoje[pozycja][1] == "" {
-        pokoje[pozycja][1] = "Forzaob"
-    }
-
-    let pozycja = wez_pozycje(6, &pokoje);
-    if pokoje[pozycja][0] == "" {
-        pokoje[pozycja][0] = "Duda"
-    } else if pokoje[pozycja][1] == "" {
-        pokoje[pozycja][1] = "Duda"
-    }
-
-    wyswietl_pokoje(&pokoje);
-    
-    for pokoj in pokoje.iter() {
-        if (pokoj[0] == "Szarak" && pokoj[1] == "Lechański") || (pokoj[1] == "Szarak" && pokoj[0] == "Lechański") {
-            println!("Lechański umarł");
-        }
-        if (pokoj[0] == "Mumin" && pokoj[1] == "Forzaob") || (pokoj[1] == "Mumin" && pokoj[0] == "Forzaob") {
-            println!("Klony się połączyły co wywołało wybuch porównalny do wybuchy bomby atomowej");
-        }
-    }
-
-    println!("Wciśnij enter aby zakończyć");
-    let mut zatrzymajka = String::new();
-    io::stdin().read_line(&mut zatrzymajka).unwrap();
-}*/
-
+#[derive(Clone, Copy)]
 struct PokojeManager<'a> {
     pokoje: [[&'a str;2];3],
 }
 
+
 impl PokojeManager<'_> {
+
+    fn r#move<'a>(&'a mut self, what: &'a str, r#where: &'a usize) {
+        for pokoj in self.pokoje.iter_mut() {
+            for mut miejsce in pokoj.iter_mut() {
+                if miejsce as &str == what {
+                    miejsce = &mut "";
+                    self.pokoje[*r#where][1] = what;
+                }
+            }
+        }
+    }
+
+    fn input_handler(&mut self) {
+        loop {
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("To nie jest ciag znakow");
+
+            let komenda = match self.interpretator(input.as_str()) {
+                Err(e) => (e as &str, "sample", 0 as usize),
+                Ok(v) => v,
+            };
+
+            match komenda.0 {
+                "move" => &mut self.r#move(komenda.1, &komenda.2),
+                _ => &mut println!("{}", komenda.0),
+            };
+        }
+    }
+
+    fn interpretator<'a>(self, input: &'a str) -> Result<(&str, &str, usize), &str> {
+        let mut iter = input.split_whitespace();
+        
+         match iter.next() {
+            None => Err("Podana komenda pusta"),
+            Some("move") => { //<(&str,usize),&str>
+                let arg1 = match iter.next() {
+                    None => Err("Brak argumentu nr 1"),
+                    Some(v) => Ok(v),
+                };
+                let arg2 = match iter.next() {
+                    None => Err("Brak argumentu nr 2"),
+                    Some(v) => Ok(v.parse::<usize>().unwrap()),
+                };
+                match arg1 {
+                    Err(e) => Err(e),
+                    Ok(v) => match arg2 {
+                        Err(e) => Err(e),
+                        Ok(dv) => Ok(("move",v,dv))
+                    }
+                }
+            },
+            _ => Err("Komenda nie istnieje"),
+        }
+        
+    }
+
     fn wez_pozycje(&self, n_zapytania: usize) -> usize {
         loop {
             let mut input = String::new();
-            dialog(n_zapytania);
+            self.dialog(n_zapytania);
             self.wyswietl_pokoje();
 
             io::stdin().read_line(&mut input).expect("To nie jest ciag znakow");
@@ -145,6 +95,19 @@ impl PokojeManager<'_> {
         }
         println!("\n--------------------------------------------");
     }
+    
+    fn dialog(&self, n: usize) {
+        let dialogi = [
+            "Jesteś managerem życia. Przed tobą 3 puste pokoje.\nMusisz zalokować 6 osób: Kromke, Szaraka, Mumina, Lechańskiego, Forzaoba i Dude.",
+            "Do jakiego pokoju chcesz wrzucić Kromke? (1,2 lub 3)",
+            "Do jakiego pokoju chcesz wrzucić Szaraka? (1,2 lub 3)",
+            "Do jakiego pokoju chcesz wrzucić Mumina? (1,2 lub 3)",
+            "Do jakiego pokoju chcesz wrzucić Lechańskiego? (1,2 lub 3)",
+            "Do jakiego pokoju chcesz wrzucić Forzaoba? (1,2 lub 3)",
+            "Do jakiego pokoju chcesz wrzucić Dude? (1,2 lub 3)",
+        ];
+        println!("{}", dialogi[n]);
+    }
 }
 
 fn main() {
@@ -153,7 +116,8 @@ fn main() {
         pokoje: pokoje,
     };
 
-    dialog(0);
+
+    game.dialog(0);
 
     let pozycja: usize = game.wez_pozycje(1);
     if game.pokoje[pozycja][0] == "" {
@@ -198,6 +162,8 @@ fn main() {
     }
 
     game.wyswietl_pokoje();
+
+    game.input_handler();
     
     for pokoj in game.pokoje.iter() {
         if (pokoj[0] == "Szarak" && pokoj[1] == "Lechański") || (pokoj[1] == "Szarak" && pokoj[0] == "Lechański") {
