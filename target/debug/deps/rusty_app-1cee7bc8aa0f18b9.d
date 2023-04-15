5bea0ced7efdb007740802ec403290ad2ed269db
patrykduda2005 <patrykduda7@gmail.com>
2023-04-15 21:00:00 +0200

    rozdzielono module na oddzielny plik

--- target/debug/deps/rusty_app-1cee7bc8aa0f18b9.d
+++ target/debug/deps/rusty_app-1cee7bc8aa0f18b9.d
@@ -1,5 +1,6 @@
-/home/duda/Code/rust/rusty_app/target/debug/deps/rusty_app-1cee7bc8aa0f18b9.rmeta: src/main.rs
+/home/duda/Code/rust/rusty_app/target/debug/deps/rusty_app-1cee7bc8aa0f18b9.rmeta: src/main.rs src/the_pokoje.rs
 
-/home/duda/Code/rust/rusty_app/target/debug/deps/rusty_app-1cee7bc8aa0f18b9.d: src/main.rs
+/home/duda/Code/rust/rusty_app/target/debug/deps/rusty_app-1cee7bc8aa0f18b9.d: src/main.rs src/the_pokoje.rs
 
 src/main.rs:
+src/the_pokoje.rs:
